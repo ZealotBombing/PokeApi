deb09acfabe438da433c33615c01ac2b038be21c
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-03-30 00:53:29 -0300

    FC

---
+++ PokeApi/Areas/Controllers/PokemonController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PokeApi.Areas.Controller
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PokemonController : ControllerBase
+    {
+        [HttpGet("Pokemon")]
+        public IEnumerable<String> GetStrings()
+        {
+            return new string[] { "Fernanda", "Piña" };
+        }
+        
+    }
+}
