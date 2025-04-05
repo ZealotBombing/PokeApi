0b9ef8127d63141f8e5043ca6012afaeb769b961
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-04-05 13:07:36 -0300

    some missing properties

--- PokemonIntegration/Component/Pokemon/DataTransferObject/PokemonApiResultDto.cs
+++ PokemonIntegration/Component/Pokemon/DataTransferObject/PokemonApiResultDto.cs
@@ -15,15 +15,18 @@ namespace PokemonIntegration.Component.Pokemon.DataTransferObject
         public bool is_default { get; set; }
         public int order { get; set; }
         public int weight { get; set; }
-        public string location_area_encounters { get; set; }
         public List<PokemonAbilityApiResultDto> abilities { get; set; }
+        public List<PokemonFormTypeApiResultDto> forms { get; set; }
 
         //TODO: game_indices
+        public List<PokemonHeldItemApiResultDto> held_items { get; set; }
+        public string location_area_encounters { get; set; }
         public List<PokemonMoveApiResultDto> moves { get; set; }
         public List<PokemonTypePastApiResultDto> past_types { get; set; }
         public PokemonSpritesApiResultDto sprites { get; set; }
 
         //TODO: stats
+        public List<PokemonCriesApiResultDto> cries { get; set; }
 
         public List<PokemonTypeApiResultDto> types { get; set; }
     }
