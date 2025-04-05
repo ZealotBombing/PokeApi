8a9920bfcfc399e1c7a5615fecbb550ccedef939
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-04-05 13:29:54 -0300

    create prokemon impl and pokemon mapper

--- PokemonIntegration/obj/Debug/net8.0/PokemonIntegration.AssemblyInfo.cs
+++ PokemonIntegration/obj/Debug/net8.0/PokemonIntegration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonIntegration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a162485488616a7db5435072ea691dbe9d43ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+246373ba752d4ad4d60ca5bd3af6a5d9cf8092cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonIntegration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonIntegration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
