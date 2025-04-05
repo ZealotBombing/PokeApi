11b7f392deead0aff553f4293b36ac7fe8ff678a
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-04-05 03:43:35 -0300

    poke dtos

--- PokeApi/obj/Debug/net8.0/PokeApi.AssemblyInfo.cs
+++ PokeApi/obj/Debug/net8.0/PokeApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+098e36e21d5a5da546aa379531b219cadbccf6be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cea82737979a9a2cb123d9ede0a0ea394876549c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PokemonIntegration/obj/Debug/net8.0/PokemonIntegration.AssemblyInfo.cs
+++ PokemonIntegration/obj/Debug/net8.0/PokemonIntegration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonIntegration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2fae78d4fca20a494ef10552de3915d9dc97ac9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cea82737979a9a2cb123d9ede0a0ea394876549c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonIntegration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonIntegration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
