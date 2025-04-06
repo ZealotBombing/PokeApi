3d46a6f06f7bda428e01fb5590e26f222af429c9
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-04-06 02:07:03 -0400

    Caché

--- Pokemon/obj/Debug/net8.0/Pokemon.AssemblyInfo.cs
+++ Pokemon/obj/Debug/net8.0/Pokemon.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokemon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f8dfe5d6d3241b67fb1ea009985069910435c1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+246373ba752d4ad4d60ca5bd3af6a5d9cf8092cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokemon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokemon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
