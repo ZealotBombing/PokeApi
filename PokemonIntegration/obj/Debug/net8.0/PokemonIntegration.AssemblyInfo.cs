9ba910b802a3e8bfb926bb4e6a3de690f25510d8
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-04-06 14:38:41 -0400

    flow tested

--- PokemonIntegration/obj/Debug/net8.0/PokemonIntegration.AssemblyInfo.cs
+++ PokemonIntegration/obj/Debug/net8.0/PokemonIntegration.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokemonIntegration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fc63899307cc59327adc27cff7e33a3a93bc3de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+706b2503df04bff3645effd4f27cde0857c8a066")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokemonIntegration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokemonIntegration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
