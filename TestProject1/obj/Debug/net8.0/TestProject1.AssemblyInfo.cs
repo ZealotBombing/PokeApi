ea06ecd0b472c8bd6a6a7a7289da6344dafbabf3
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-04-11 21:17:18 -0400

    Nlog in BLL

--- TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net8.0/TestProject1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84eed2f1af2b8ac56e30fdba905a7ba2334a2ef3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95ddb40b72b5d89fa5f46815a7f33f860f9d0993")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
