46a7a2c9898550a040cbc6ddc7c3a3dcfd41c511
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-11-15 01:59:16 -0300

    add front

---
+++ BACKEND/Pokemon/Component/BusinessMapper/PokemonTypeMapper.cs
@@ -0,0 +1,27 @@
+﻿using Pokemon.Component.DataTransferObject;
+using PokemonIntegration.Component.Pokemon.DataTransferObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon.Component.BusinessMapper
+{
+    internal static class PokemonTypeMapper
+    {
+        public static List<PokemonTypeDto> ToDto(List<PokemonTypeApiResultDto> pokemonTypes)
+        {
+            return pokemonTypes.Select(ToDto).ToList();
+        }
+
+        public static PokemonTypeDto ToDto(PokemonTypeApiResultDto dto)
+        {
+            var pokemonType = new PokemonTypeDto();
+
+            pokemonType.Slot = dto.slot;
+            
+            return pokemonType;
+        }
+    }
+}
